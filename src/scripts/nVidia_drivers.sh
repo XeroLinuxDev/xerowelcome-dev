f18f2fa0e22ea8aa65ec52851318373ccdd04b69
TechXero <steve@techxero.com>
2023-05-22 11:53:26 +0300

    nVidia

--- src/scripts/nVidia_drivers.sh
+++ src/scripts/nVidia_drivers.sh
@@ -14,7 +14,7 @@ echo "#                             !!! ATTENTION !!!                           
 echo "#                                                                             #"
 echo "#                     Carefully Select The right Drivers..                    #"
 echo "#                                                                             #"
-echo "#     Only R530.x & R470.x Drivers will be Available. For Older Check TKG     #"
+echo "#   Latest R525.x/R530.x & R470.x Drivers are provided. For Older Check TKG   #"
 echo "#           TKG Git -> https://github.com/Frogging-Family/nvidia-all          #"
 echo "###############################################################################"
 tput sgr0
@@ -27,8 +27,9 @@ echo "1.  Check Which nVidia GPU You Have."
 echo
 echo "############# nVidia Proprietary Drivers #############"
 echo
-echo "2.  nVidia R530.x Drivers (GTX 900 Seies & Newer incl. 745/750ti)."
-echo "3.  nVidia R470.x Drivers (GTX 600 & 700 Series excl. 605/610/620/625/645)."
+echo "2.  R530.x Drivers (New Features Branch)."
+echo "3.  R525.x Drivers (Production Branch)."
+echo "4.  R470.x Drivers (Legacy Branch)."
 echo
 echo "Type Your Selection. To Exit, just close Window."
 echo
@@ -70,6 +71,15 @@ case $CHOICE in
       ;;
 
     2 )
+      
+      echo
+      echo "####################################################"
+      echo "              New Feature Branch (NFB)              "
+      echo "                                                    "
+      echo "  Provide early adopters access to latest features  "
+      echo "     before merging to the Production Branches      "
+      echo "####################################################"
+      sleep 10
       echo
       echo "##########################################"
       echo "   Installing nVidia R530.x GPU Drivers   "
@@ -84,6 +94,36 @@ case $CHOICE in
       ;;
 
     3 )
+      echo
+      echo "####################################################"
+      echo "               Production Branch (PB)               "
+      echo "                                                    "
+      echo "      Provides optimal stability & performance      "
+      echo "   Receives sustained bug fix & security updates.   "
+      echo "####################################################"
+      sleep 10
+      echo
+      echo "##########################################"
+      echo "   Installing nVidia R525.x GPU Drivers   "
+      echo "##########################################"
+      sleep 3
+      sudo pacman -S --noconfirm nvidia-525xx-dkms-tkg nvidia-525xx-utils-tkg nvidia-525xx-settings-tkg nvidia-525xx-egl-wayland-tkg opencl-nvidia-525xx-tkg libxnvctrl-525xx lib32-nvidia-525xx-utils-tkg lib32-opencl-nvidia-525xx-tkg dxvk-nvapi-mingw dxvk-bin
+      sleep 3
+      echo "#######################################"
+      echo "                 Done !                "
+      echo "#######################################"
+            exit
+      ;;
+
+    4 )
+      echo
+      echo "####################################################"
+      echo "                 Legacy Branch (LB)                 "
+      echo "                                                    "
+      echo "       Provides Support for now Legacy GPUs         "
+      echo "    Rarely receives any updates. Use at Own Risk    "
+      echo "####################################################"
+      sleep 10
       echo
       echo "##########################################"
       echo "   Installing nVidia R470.x GPU Drivers   "
