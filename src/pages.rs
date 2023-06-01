0f6dfcc28fde4debd92de9cb0613cc71e9c40d4e
TeddyBearKilla <noobilla@protonmail.com>
2023-06-01 11:59:36 -0400

    Libadw & tooltips

--- src/pages.rs
+++ src/pages.rs
@@ -375,15 +375,17 @@ pub fn init_gpg_main_button(builder: &Builder) {
 pub fn init_update_sys_main_button(builder: &Builder) {
     let update_system_btn: gtk::Button = builder.object("update-system").unwrap();
     update_system_btn.connect_clicked(on_update_system_btn_clicked);
+        ):
+    });
 }
 
 pub fn init_libadw_main_button(builder: &Builder) {
-    let fix_keys_btn: gtk::Button = builder.object("libadw").unwrap();
-    fix_keys_btn.connect_clicked(move |_| {
+    let libadw_btn: gtk::Button = builder.object("libadw").unwrap();
+    libadw_btn.connect_clicked(move |_| {
     let _ = utils::run_cmd_terminal(
         String::from("/usr/share/xerowelcome/scripts/fix_gtk4.sh"),
         false,
-    );
+    });
 }
 
 fn on_paconf_btn_clicked(_: &gtk::Button) {
