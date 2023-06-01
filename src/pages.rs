5bc1635f26dd75c71016033909514567a6c8106a
TeddyBearKilla <noobilla@protonmail.com>
2023-06-01 13:31:22 -0400

    Libadw

--- src/pages.rs
+++ src/pages.rs
@@ -372,6 +372,16 @@ pub fn init_gpg_main_button(builder: &Builder) {
     });
 }
 
+pub fn init_adw_main_button(builder: &Builder) {
+    let fix_gtk4_btn: gtk::Button = builder.object("fix-adw-gtk4").unwrap();
+    fix_gtk4_btn.connect_clicked(move |_| {
+        let _ = utils::run_cmd_terminal(
+            String::from("/usr/share/xerowelcome/scripts/fix_gtk4.sh"),
+            false,
+        );
+    });
+}
+
 pub fn init_update_sys_main_button(builder: &Builder) {
     let update_system_btn: gtk::Button = builder.object("update-system").unwrap();
     update_system_btn.connect_clicked(on_update_system_btn_clicked);
